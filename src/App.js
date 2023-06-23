25aa0e32d31012862b4fe9edd994c18b6136456c
Amulya Parab <amulyaparab101@gmail.com>
2023-06-23 20:45:54 +0530

    feat: set up table, and context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import "./App.css";
+import { snacks } from "./Database/snacks";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Snack Table</h1>
+      <input placeholder="Search with Products or Ingredients..." />
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Product Name</th>
+          <th>Product Weight</th>
+          <th>Price (INR)</th>
+          <th>Calories</th>
+          <th>Ingredients</th>
+        </tr>
+
+        {snacks.map(
+          ({
+            id,
+            product_name,
+            product_weight,
+            price,
+            calories,
+            ingredients,
+          }) => (
+            <tr>
+              <td>{id}</td>
+              <td>{product_name}</td>
+              <td>{product_weight}</td>
+              <td>{price}</td>
+              <td>{calories}</td>
+              <td>{`${ingredients}  `}</td>
+            </tr>
+          )
+        )}
+      </table>
+    </div>
+  );
 }
 
 export default App;
