654eee70b87f65b3cacb78b67eb9feb388a919bb
Amulya Parab <amulyaparab101@gmail.com>
2023-06-23 21:51:30 +0530

    feat: added sorting functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
 import "./App.css";
+import { useData } from "./Context/DataProvider";
 import { snacks } from "./Database/snacks";
 
 function App() {
+  const { state, dispatch, ascending, setAscending } = useData();
   return (
     <div className="App">
       <h1>Snack Table</h1>
-      <input placeholder="Search with Products or Ingredients..." />
+      <input
+        placeholder="Search with Products or Ingredients..."
+        value={state?.search}
+        onChange={(event) =>
+          dispatch({ type: "SEARCH_BY", payload: event.target.value })
+        }
+      />
       <table>
         <tr>
-          <th>ID</th>
-          <th>Product Name</th>
-          <th>Product Weight</th>
-          <th>Price (INR)</th>
-          <th>Calories</th>
-          <th>Ingredients</th>
+          <th
+            onClick={() => {
+              dispatch({ type: "SORT_BY_ID" });
+              setAscending(!ascending);
+            }}
+          >
+            ID
+          </th>
+          <th
+            onClick={() => {
+              dispatch({ type: "SORT_BY_PRODUCT_NAME" });
+              setAscending(!ascending);
+            }}
+          >
+            Product Name
+          </th>
+          <th
+            onClick={() => {
+              dispatch({ type: "SORT_BY_PRODUCT_WEIGHT" });
+              setAscending(!ascending);
+            }}
+          >
+            Product Weight
+          </th>
+          <th
+            onClick={() => {
+              dispatch({ type: "SORT_BY_PRICE" });
+              setAscending(!ascending);
+            }}
+          >
+            Price (INR)
+          </th>
+          <th
+            onClick={() => {
+              dispatch({ type: "SORT_BY_CALORIES" });
+              setAscending(!ascending);
+            }}
+          >
+            Calories
+          </th>
+          <th
+            onClick={() => {
+              dispatch({ type: "SORT_BY_INGREDIENTS" });
+              setAscending(!ascending);
+            }}
+          >
+            Ingredients
+          </th>
         </tr>
 
-        {snacks.map(
+        {state.filteredData.map(
           ({
             id,
             product_name,
@@ -25,7 +75,7 @@ function App() {
             calories,
             ingredients,
           }) => (
-            <tr>
+            <tr key={id}>
               <td>{id}</td>
               <td>{product_name}</td>
               <td>{product_weight}</td>
--- src/Context/DataProvider.js
+++ src/Context/DataProvider.js
@@ -1,7 +1,127 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
+import { snacks } from "../Database/snacks";
 
 const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
-  return <DataContext.Provider value={{}}>{children}</DataContext.Provider>;
+  const [ascending, setAscending] = useState(true);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SEARCH_BY":
+        return {
+          ...state,
+          search: action.payload,
+          filteredData: state.snacks.filter(({ product_name, ingredients }) => {
+            console.log(
+              ingredients.includes(
+                ingredients.filter((ingredient) =>
+                  ingredient.includes(action.payload)
+                )
+              )
+            );
+            return product_name
+              .toLowerCase()
+              .includes(action.payload.toLowerCase());
+          }),
+        };
+
+      case "SORT_BY_ID":
+        return {
+          ...state,
+          filteredData: state?.filteredData?.sort((a, b) =>
+            ascending ? b.id - a.id : a.id - b.id
+          ),
+        };
+      case "SORT_BY_PRODUCT_NAME":
+        return {
+          ...state,
+          filteredData: state?.filteredData?.sort((a, b) => {
+            if (ascending) {
+              if (a.product_name < b.product_name) {
+                return -1;
+              } else if (a.product_name > b.product_name) {
+                return 1;
+              } else {
+                return 0;
+              }
+            } else {
+              if (b.product_name < a.product_name) {
+                return -1;
+              } else if (b.product_name > a.product_name) {
+                return 1;
+              } else {
+                return 0;
+              }
+            }
+          }),
+        };
+      case "SORT_BY_PRODUCT_WEIGHT":
+        return {
+          ...state,
+          filteredData: state?.filteredData?.sort((a, b) =>
+            ascending
+              ? b.product_weight.slice(0, -1) - a.product_weight.slice(0, -1)
+              : a.product_weight.slice(0, -1) - b.product_weight.slice(0, -1)
+          ),
+        };
+      case "SORT_BY_PRICE":
+        return {
+          ...state,
+          filteredData: state?.filteredData?.sort((a, b) =>
+            ascending ? b.price - a.price : a.price - b.price
+          ),
+        };
+      case "SORT_BY_CALORIES":
+        return {
+          ...state,
+          filteredData: state?.filteredData?.sort((a, b) =>
+            ascending ? b.calories - a.calories : a.calories - b.calories
+          ),
+        };
+      case "SORT_BY_INGREDIENTS":
+        return {
+          ...state,
+          filteredData: state?.filteredData?.sort((a, b) => {
+            if (ascending) {
+              if (a.ingredients[0] < b.ingredients[0]) {
+                return -1;
+              } else if (a.ingredients[0] > b.ingredients[0]) {
+                return 1;
+              } else {
+                return 0;
+              }
+            } else {
+              if (b.ingredients[0] < a.ingredients[0]) {
+                return -1;
+              } else if (b.ingredients[0] > a.ingredients[0]) {
+                return 1;
+              } else {
+                return 0;
+              }
+            }
+          }),
+        };
+      default:
+        return state;
+    }
+  };
+  const initialState = {
+    snacks: snacks,
+    filteredData: snacks,
+    search: "",
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <DataContext.Provider
+      value={{
+        dispatch,
+        state,
+        ascending,
+        setAscending,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
 };
+export const useData = () => useContext(DataContext);
